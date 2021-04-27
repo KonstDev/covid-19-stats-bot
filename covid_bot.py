b227926ce466a6b1a463f6cc196db17d2d3228dd
Kostya Khushensky <46225922+KonstDev@users.noreply.github.com>
2021-04-27 20:50:25 +0300

    Add files via upload
    added chanching stats

--- covid_bot.py
+++ covid_bot.py
@@ -9,7 +9,7 @@ from time import sleep
 import random
 from covid.api import CovId19Data
 
-api = CovId19Data(force=False)
+
 
 app = Client("my_account")
 
@@ -19,20 +19,36 @@ hou_r = 0;
 
 
 
-print('YES')
+#print('YES')
+
+got_it = False
+
+#global previos
+
+
 @app.on_message(filters.command("start", prefixes=".") & filters.me)
 def parse(_, msg):
-	app.send_message(chat_id, 'Bot started!' + str(datetime.datetime.now()))
+	previos = 352950 #351674
+	app.send_message(chat_id, 'Bot started! :' + str(datetime.datetime.now()))
 	while(True):
+		api = CovId19Data(force=False)
 		timebnow = datetime.datetime.now()
-		if timebnow.minute == 0 and timebnow.second == 0:# and timebnow.hour == hou_r: #True: 
+		if timebnow.minute == 40: # and timebnow.second == 0:# and timebnow.hour == hou_r: #True: 
 		    	location = res = api.filter_by_country('belarus')
 		    	recovered = location['recovered']
 		    	deaths = location['deaths']
 		    	confirmed = location['confirmed']
-		    	message = 'Данные на сегодня: ' + str(confirmed) + ' подтвержденных случаев, ' + str(deaths) + ' смерть(ей), ' + str(confirmed) + ' случаев выздоровления.';
+		    	#previos = previos1
+		    	message = 'Данные на сегодня: ' + str(confirmed) + ' подтвержденных случаев, ' + str(deaths) + ' смерть(ей), ' + str(recovered) + ' случаев выздоровления.';
+		    	#if previos != 0 and not (previos == confirmed):
+		    	message += '\n' + 'За сутки выявлено ' + str(confirmed - previos) + ' новых случаев.'
+		    	hour = datetime.datetime.now().hour
+		    	if (hour == 23):
+		    		previos = confirmed
 		    	app.send_message(chat_id, message)
-		    	sleep(10)
+		    	sleep(50)
+		else:
+			sleep(50)
 	    #msg.edit("YES, CAPTAIN! SPAMMING " + str(n) + " TIMES")
 
 app.run()
